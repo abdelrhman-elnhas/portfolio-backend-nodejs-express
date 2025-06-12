18191952505e7e1e3a479cee3604e829bb422588
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2025-06-12 20:00:08 +0300

    db configuration fix

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "mysql",
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: true, // important for Railway
-    },
-  },
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // Set in .env
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
