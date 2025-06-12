c8489c28a5c226cf56b76bedaee97b0cff2828a4
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2025-06-12 19:43:58 +0300

    db configuration

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: 18847, // Railway MySQL port
+    port: process.env.DB_PORT || 3306, // ✅ use env var first
     dialect: "mysql",
     logging: false,
   }
