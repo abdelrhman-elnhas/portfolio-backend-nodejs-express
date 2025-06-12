cf7290abc840fed92f6b9d9c9e4246e2ffdd3faf
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2025-06-12 20:06:36 +0300

    db configuration fix

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // Set in .env
+    port: process.env.DB_PORT, // <- string is fine, Sequelize handles it
     dialect: "mysql",
     logging: false,
   }
